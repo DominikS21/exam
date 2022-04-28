0a6de353120842c6be18f46f96790d622123c5bb
DominikS21 <dominik.sabak@live.zs1mm.edu.pl>
2022-04-28 08:51:10 +0200

    test

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exam</title>
 </head>
 <body>
     <h1> Strona w trakcie tworzenia</h1>
     <?php
-
     echo(date("Y-m-d"));
     echo "<br>";
     echo(date("h:i:s"))
