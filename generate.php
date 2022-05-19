3a22ffc35638af4145ffde60c003fcae7517d366
DominikS21 <95407922+DominikS21@users.noreply.github.com>
2022-05-19 10:53:00 +0200

    0.2

--- generate.php
+++ generate.php
@@ -17,9 +17,11 @@ foreach(range(1,20) as $x) {
   $name = $faker->name;
   $pattern = '/ /';
   $replacement = '_';
-  $imagefile = preg_replace($pattern, $replacement, $name);
-#  $conn->query("
- #   INSERT INTO images (name, picsum_id, imagefile author, width, height, added_at) VALUES ('$name', '$losowa', '$imagefile', '{$contentarray->author}', '$contentarray->width', '$contentarray->height', '$data')
- #   ");
+  $imagefil = preg_replace($pattern, $replacement, $name);
+  $imagefile = $imagefil.'.jpg';
+  file_put_contents('./images/'.$imagefile, $content);
+  $conn->query("
+    INSERT INTO images (name, picsum_id, imagefile, author, width, height, added_at) VALUES ('$name', '$losowa', '$imagefile', '{$contentarray->author}', '$contentarray->width', '$contentarray->height', '$data')
+    ");
 }
 ?>
